0a978dabda65a273fed3da8614c2fecbec2a76bf
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-09-02 20:45:34 -0300

    Comencé el ejercicio Promedio de notas

---
+++ 1- APIs con Spring Boot/promedioDeNotas/src/test/java/com/example/promedioDeNotas/PromedioDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promedioDeNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromedioDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
