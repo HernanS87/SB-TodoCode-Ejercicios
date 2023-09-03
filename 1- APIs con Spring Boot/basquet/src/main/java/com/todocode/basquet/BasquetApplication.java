9fc2e4e94a8c54be84ad80b02b62b0fdacc8ed3e
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-09-03 12:27:45 -0300

    Comencé el ejercicio de Basquet

---
+++ 1- APIs con Spring Boot/basquet/src/main/java/com/todocode/basquet/BasquetApplication.java
@@ -0,0 +1,13 @@
+package com.todocode.basquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasquetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasquetApplication.class, args);
+	}
+
+}
---
+++ 1- APIs con Spring Boot/basquet/src/test/java/com/todocode/basquet/BasquetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocode.basquet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasquetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
