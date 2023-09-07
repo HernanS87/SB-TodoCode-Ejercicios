1cbc34e3f9b91ee426c3e317495e24e9e436949b
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-09-07 20:53:00 -0300

    Creé los métodos CRUD para PersonaService

--- 2- CRUD con JPA + Hibernate/jpaDemo/src/main/java/com/todocode/jpaDemo/service/IPersonaService.java
+++ 2- CRUD con JPA + Hibernate/jpaDemo/src/main/java/com/todocode/jpaDemo/service/IPersonaService.java
@@ -1,6 +1,23 @@
 package com.todocode.jpaDemo.service;
 
+import com.todocode.jpaDemo.entity.Persona;
+import java.util.List;
+
 
 public interface IPersonaService {
+    
+    public List<Persona> getPersonas();
+    
+    public Persona findPersona(Long id);
+    
+    public void savePersona(Persona p);
+    
+    public void deletePersona(Long id);
+    
+    public void editPersona(Long idOriginal, 
+                            Long idNuevo, 
+                            String nombreNuevo, 
+                            String apellidoNuevo, 
+                            int edadNueva);
     
 }
--- 2- CRUD con JPA + Hibernate/jpaDemo/src/main/java/com/todocode/jpaDemo/service/PersonaService.java
+++ 2- CRUD con JPA + Hibernate/jpaDemo/src/main/java/com/todocode/jpaDemo/service/PersonaService.java
@@ -1,13 +1,49 @@
 package com.todocode.jpaDemo.service;
 
+import com.todocode.jpaDemo.entity.Persona;
 import com.todocode.jpaDemo.repository.IPersonaRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PersonaService {
+public class PersonaService implements IPersonaService{
     
     @Autowired
     private IPersonaRepository persoRepo;
+
+    @Override
+    public List<Persona> getPersonas() {
+        List<Persona> listaPersonas = persoRepo.findAll();
+        return listaPersonas;
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+       Persona p = persoRepo.findById(id).orElse(null);
+       return p;
+    }
+
+    @Override
+    public void savePersona(Persona p) {
+        persoRepo.save(p);
+    }
+
+    @Override
+    public void deletePersona(Long id) {
+        persoRepo.deleteById(id);
+    }
+
+    @Override
+    public void editPersona(Long idOriginal, Long idNuevo, String nombreNuevo, String apellidoNuevo, int edadNueva) {
+        Persona p = this.findPersona(idOriginal);
+        
+        p.setId(idNuevo);
+        p.setNombre(nombreNuevo);
+        p.setApellido(apellidoNuevo);
+        p.setEdad(edadNueva);
+        
+        this.savePersona(p);
+    }
     
 }
