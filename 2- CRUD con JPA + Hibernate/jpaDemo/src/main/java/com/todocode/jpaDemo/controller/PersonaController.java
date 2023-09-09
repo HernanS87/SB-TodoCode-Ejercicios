2e564d4938f071937d2ad87a2b69d7e2efbc9b9d
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-09-08 22:19:28 -0300

    Creé PersonaController con todos los endPoints necesarios para el CRUD

---
+++ 2- CRUD con JPA + Hibernate/jpaDemo/src/main/java/com/todocode/jpaDemo/controller/PersonaController.java
@@ -0,0 +1,57 @@
+package com.todocode.jpaDemo.controller;
+
+import com.todocode.jpaDemo.entity.Persona;
+import com.todocode.jpaDemo.service.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonaController {
+    
+    @Autowired
+    IPersonaService personaServ;
+    
+    @GetMapping
+    public String inicio(){
+        return "JPA DEMO!!! ❤️";
+    }
+    
+    @GetMapping("/personas/traer")
+    public List<Persona> getPersonas(){
+        return personaServ.getPersonas();
+    }
+    
+    @PostMapping("/personas/crear")
+    public String savePersona(@RequestBody Persona p){
+        personaServ.savePersona(p);
+        return "La persona fue creada correctamente";
+    }
+    
+    @DeleteMapping("/personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        personaServ.deletePersona(id);
+        return "La persona fue eliminada correctamente";
+    }
+    
+    @PutMapping("/personas/editar/{idOriginal}")
+    public Persona editPersona(@PathVariable Long idOriginal,
+                               @RequestParam(required = false, name = "id") Long idNuevo,
+                               @RequestParam(required = false, name = "nombre") String nombreNuevo,
+                               @RequestParam(required = false, name = "apellido") String apellidoNuevo,
+                               @RequestParam(required = false, name = "edad") int edadNueva){
+        
+        personaServ.editPersona(idOriginal, idNuevo, nombreNuevo, apellidoNuevo, edadNueva);
+        
+        return personaServ.findPersona(idNuevo);
+        
+    } 
+    
+}
