a798573756b0b3662342fa69ab5c88a8645e1bc9
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-09-09 17:01:53 -0300

    Creé la clase Mascota y modifiqué los GenerationType.SEQUENCE, para que tenga sólo una tabla de sequencia global y no una por cada entidad

--- 2- CRUD con JPA + Hibernate/jpaDemo/src/main/java/com/todocode/jpaDemo/entity/Mascota.java
+++ 2- CRUD con JPA + Hibernate/jpaDemo/src/main/java/com/todocode/jpaDemo/entity/Mascota.java
@@ -1,39 +1,35 @@
 package com.todocode.jpaDemo.entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 @Entity
-public class Persona {
+public class Mascota {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hibernate_sequence")
     @SequenceGenerator(name = "hibernate_sequence", allocationSize = 1)
-    private Long id;
+    private Long id_mascota;
     private String nombre;
-    private String apellido;
-    private int edad;
-    @OneToOne
-    @JoinColumn(name="id_mascota")
-    private Mascota mascota;
+    private String especie;
+    private String raza;
+    private String color;
 
-    public Persona() {
+    public Mascota() {
     }
 
-    public Persona(Long id, String nombre, String apellido, int edad) {
-        this.id = id;
+    public Mascota(String nombre, String especie, String raza, String color) {
         this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
+        this.especie = especie;
+        this.raza = raza;
+        this.color = color;
     }
-       
+    
     
 }
