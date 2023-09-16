63e9b0524cedfc6ef94e7a6e793dbef438cbf423
Hernan Santarelli <hernansantarelli87@gmail.com>
2023-09-16 20:33:20 -0300

    Creé la clase Tema. La relacioné con Curso e implementé la notation @Data de lombok

--- 2- CRUD con JPA + Hibernate/estudiante/src/main/java/com/todocode/estudiante/entity/Tema.java
+++ 2- CRUD con JPA + Hibernate/estudiante/src/main/java/com/todocode/estudiante/entity/Tema.java
@@ -4,28 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
-import java.time.LocalDate;
-import java.util.List;
 import lombok.Data;
-
 
 @Data
 @Entity
-public class Curso {
+public class Tema {
+  
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hibernate_seq")
   @SequenceGenerator(name = "hibernate_seq", allocationSize = 1)
-  private Long id;
   private String nombre;
-  private String modalidad;
-  private LocalDate fecha_finalizacion;
-  
-  @OneToMany
-  @JoinColumn(name = "id_curso")
-  private List<Tema> lista_temas;
+  private String descripcion;
 
-  
 }
